7ce387ff83fb40541fdeeb482c6890086b3ec694
yasutomo <yasutomo@192.168.0.15>
2015-02-21 17:16:04 +0900

    不要と思われるディレクトリの削除

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -10,9 +10,9 @@ LOCAL_MODULE := cocos2dcpp_shared
 
 LOCAL_MODULE_FILENAME := libcocos2dcpp
 
-CPP_FILES := $(shell find $(LOCAL_PATH)/../../Classes -name *.cpp)
-LOCAL_SRC_FILES := hellocpp/main.cpp
-LOCAL_SRC_FILES += $(CPP_FILES:$(LOCAL_PATH)/%=%)
+LOCAL_SRC_FILES := hellocpp/main.cpp \
+                   ../../Classes/AppDelegate.cpp \
+                   ../../Classes/HelloWorldScene.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
